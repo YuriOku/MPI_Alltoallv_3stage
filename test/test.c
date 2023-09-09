614b06286ca7116b7c6be9ddfa373ff60fb6df2f
Yuri Oku <yuri.oku.astro@gmail.com>
2023-09-09 21:29:02 +0900

    updated test

--- test/test.c
+++ test/test.c
@@ -35,6 +35,9 @@
 
 #include "../mpi_alltoallv_3stage.h"
 
+#define LEVELMAX 15
+#define ITERS 10
+
 int main(int argc, char **argv)
 {
   MPI_Init(&argc, &argv);
@@ -49,70 +52,190 @@ int main(int argc, char **argv)
     {
       use_3stage = atoi(argv[1]);
     }
-  if(rank == 0)
-    printf("use_3stage = %d\n", use_3stage);
 
-  int n           = 5;
+  if(rank == 0)
+    {
+      printf("# use_3stage = %d\n", use_3stage);
+      printf("# size = %d\n", size);
+      printf("#\n# uniform data size\n");
+      printf("# data size    latency [us]    bandwidth [MB/s]\n");
+    }
+
   int *sendcounts = (int *)malloc(sizeof(int) * size);
   int *recvcounts = (int *)malloc(sizeof(int) * size);
   int *sdispls    = (int *)malloc(sizeof(int) * size);
   int *rdispls    = (int *)malloc(sizeof(int) * size);
+  int *sendbuf    = NULL;
+  int *recvbuf    = NULL;
+  int success_all = 1;
 
-  for(i = 0; i < size; ++i)
+  int level = 0;
+  for(level = 0; level < LEVELMAX; level++)
     {
-      sendcounts[i] = n;
-      recvcounts[i] = n;
-      sdispls[i]    = i * n;
-      rdispls[i]    = i * n;
-    }
+      int n = 1 << level;
 
-  int *sendbuf = (int *)malloc(sizeof(int) * n * size);
-  int *recvbuf = (int *)malloc(sizeof(int) * n * size);
-
-  for(i = 0; i < size; ++i)
-    {
-      for(j = 0; j < n; ++j)
+      for(i = 0; i < size; ++i)
         {
-          sendbuf[i * n + j] = rank * size + i;
+          sendcounts[i] = n;
+          recvcounts[i] = n;
+          sdispls[i]    = i * n;
+          rdispls[i]    = i * n;
         }
-    }
 
-  double t1 = MPI_Wtime();
-  if(use_3stage == 1)
-    MPI_Alltoallv_3stage(sendbuf, sendcounts, sdispls, MPI_INT, recvbuf, recvcounts, rdispls, MPI_INT, MPI_COMM_WORLD);
-  else if(use_3stage == 2)
-    MPI_Alltoallv_3stage_shared(sendbuf, sendcounts, sdispls, MPI_INT, recvbuf, recvcounts, rdispls, MPI_INT, MPI_COMM_WORLD);
-  else
-    MPI_Alltoallv(sendbuf, sendcounts, sdispls, MPI_INT, recvbuf, recvcounts, rdispls, MPI_INT, MPI_COMM_WORLD);
-  double t2 = MPI_Wtime();
+      sendbuf = (int *)malloc(sizeof(int) * n * size);
+      recvbuf = (int *)malloc(sizeof(int) * n * size);
 
-  int success = 1;
-  for(i = 0; i < size; ++i)
-    {
-      for(j = 0; j < n; ++j)
+      int iter;
+      double time = 0.0;
+      for(iter = 0; iter < ITERS; iter++)
         {
-          if(recvbuf[i * n + j] != i * size + rank)
+          for(i = 0; i < size; ++i)
             {
-              printf("rank %d: recvbuf[%d] = %d, expected %d\n", rank, i * n + j, recvbuf[i * n + j], i * size + rank);
-              success = 0;
+              for(j = 0; j < n; ++j)
+                {
+                  sendbuf[i * n + j] = rank * size + i;
+                }
+            }
+
+          double t1 = MPI_Wtime();
+          if(use_3stage == 1)
+            MPI_Alltoallv_3stage(sendbuf, sendcounts, sdispls, MPI_INT, recvbuf, recvcounts, rdispls, MPI_INT, MPI_COMM_WORLD);
+          else if(use_3stage == 2)
+            MPI_Alltoallv_3stage_shared(sendbuf, sendcounts, sdispls, MPI_INT, recvbuf, recvcounts, rdispls, MPI_INT, MPI_COMM_WORLD);
+          else
+            MPI_Alltoallv(sendbuf, sendcounts, sdispls, MPI_INT, recvbuf, recvcounts, rdispls, MPI_INT, MPI_COMM_WORLD);
+          double t2 = MPI_Wtime();
+
+          time += t2 - t1;
+
+          int success = 1;
+          for(i = 0; i < size; ++i)
+            {
+              for(j = 0; j < n; ++j)
+                {
+                  if(recvbuf[i * n + j] != i * size + rank)
+                    {
+                      printf("rank %d: recvbuf[%d] = %d, expected %d\n", rank, i * n + j, recvbuf[i * n + j], i * size + rank);
+                      success = 0;
+                    }
+                }
+            }
+
+          MPI_Allreduce(MPI_IN_PLACE, &success, 1, MPI_INT, MPI_MIN, MPI_COMM_WORLD);
+
+          if(success == 0 && rank == 0)
+            {
+              printf("test failed\n");
+              success_all = 0;
             }
         }
+
+      free(sendbuf);
+      free(recvbuf);
+
+      time /= ITERS;
+
+      double latency   = time * 1e6;
+      double bandwidth = (size * n * sizeof(int)) / time / 1e6;
+      if(rank == 0)
+        printf("%7lu %18.5f %18.5f\n", n * sizeof(int), latency, bandwidth);
     }
 
-  MPI_Allreduce(MPI_IN_PLACE, &success, 1, MPI_INT, MPI_MIN, MPI_COMM_WORLD);
-  if(success == 1 && rank == 0)
-    {
-      printf("test passed\n");
-    }
-  else if(success == 0 && rank == 0)
-    {
-      printf("test failed\n");
-    }
+  if(success_all == 1 && rank == 0)
+    printf("passed all tests\n");
 
-  double latency   = (t2 - t1) / (size * n);
-  double bandwidth = (size * n * sizeof(int)) / (t2 - t1) / 1e6;
   if(rank == 0)
-    printf("rank %d: latency = %e, bandwidth = %e\n", rank, latency, bandwidth);
+    {
+      printf("#\n# non-uniform data size\n");
+      printf("# data size    latency [us]    bandwidth [MB/s]\n");
+    }
+  success_all = 1;
+
+  for(level = 0; level < LEVELMAX; level++)
+    {
+      int n = 1 << level;
+
+      int iter;
+      double time = 0.0;
+      for(iter = 0; iter < ITERS; iter++)
+        {
+          int sdisp = 0, rdisp = 0;
+          for(i = 0; i < size; ++i)
+            {
+              double r      = (double)rand() / RAND_MAX;
+              sendcounts[i] = (r > 0.5) ? (int)(n * 4 * (r - 0.5)) : 0;
+              sdispls[i]    = sdisp;
+              sdisp += sendcounts[i];
+            }
+          MPI_Alltoall(sendcounts, 1, MPI_INT, recvcounts, 1, MPI_INT, MPI_COMM_WORLD);
+          for(i = 0; i < size; ++i)
+            {
+              rdispls[i] = rdisp;
+              rdisp += recvcounts[i];
+            }
+
+          sendbuf = (int *)malloc(sizeof(int) * sdisp);
+          recvbuf = (int *)malloc(sizeof(int) * rdisp);
+
+          for(i = 0; i < size; ++i)
+            {
+              for(j = 0; j < sendcounts[i]; ++j)
+                {
+                  sendbuf[sdispls[i] + j] = rank * size + i;
+                }
+            }
+
+          double t1 = MPI_Wtime();
+          if(use_3stage == 1)
+            MPI_Alltoallv_3stage(sendbuf, sendcounts, sdispls, MPI_INT, recvbuf, recvcounts, rdispls, MPI_INT, MPI_COMM_WORLD);
+          else if(use_3stage == 2)
+            MPI_Alltoallv_3stage_shared(sendbuf, sendcounts, sdispls, MPI_INT, recvbuf, recvcounts, rdispls, MPI_INT, MPI_COMM_WORLD);
+          else
+            MPI_Alltoallv(sendbuf, sendcounts, sdispls, MPI_INT, recvbuf, recvcounts, rdispls, MPI_INT, MPI_COMM_WORLD);
+          double t2 = MPI_Wtime();
+
+          time += t2 - t1;
+
+          int success = 1;
+          for(i = 0; i < size; ++i)
+            {
+              for(j = 0; j < recvcounts[i]; ++j)
+                {
+                  if(recvbuf[rdispls[i] + j] != i * size + rank)
+                    {
+                      printf("rank %d: recvbuf[%d] = %d, expected %d\n", rank, rdispls[i] + j, recvbuf[rdispls[i] + j], i * size + rank);
+                      success = 0;
+                    }
+                }
+            }
+
+          MPI_Allreduce(MPI_IN_PLACE, &success, 1, MPI_INT, MPI_MIN, MPI_COMM_WORLD);
+
+          if(success == 0 && rank == 0)
+            {
+              printf("test failed\n");
+              success_all = 0;
+            }
+        }
+
+      free(sendbuf);
+      free(recvbuf);
+
+      time /= ITERS;
+
+      double latency   = time * 1e6;
+      double bandwidth = (size * n * sizeof(int)) / time / 1e6;
+      if(rank == 0)
+        printf("%7lu %18.5f %18.5f\n", n * sizeof(int), latency, bandwidth);
+    }
+
+  free(sendcounts);
+  free(recvcounts);
+  free(sdispls);
+  free(rdispls);
+
+  if(success_all == 1 && rank == 0)
+    printf("passed all tests\n");
 
   MPI_Finalize();
 
